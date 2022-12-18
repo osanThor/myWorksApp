f2c34306d0554837fb3ab7392b6c0994befff85d
junyoung <jadw9715@gmail.com>
2022-12-18 16:45:13 +0900

    fix. import intro

--- src/pages/IndexPage/introSection.tsx
+++ src/pages/IndexPage/introSection.tsx
@@ -4,7 +4,6 @@ import Section from '../../components/common/section';
 import IntroDescript from '../../components/intro/introDescript';
 import Introdeuce from '../../components/intro/introdeuce';
 import MainTxt from '../../components/intro/mainTxt';
-import MouseCanvase from '../../components/intro/mouseCanvase';
 
 const IntroSection = () => {
   return (
