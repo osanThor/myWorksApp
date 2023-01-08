73e54050529076f23b451468307db1b172888e9a
junyoung <jadw9715@gmail.com>
2023-01-09 00:45:45 +0900

    feat. layout change

--- src/components/skills/skillsLayout.tsx
+++ src/components/skills/skillsLayout.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const TAGS = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Typescript',
+  'Tailwind',
+  'React',
+  'Next.js',
+  'Gatsby',
+  'UI/UX',
+  'SVG',
+  'animation',
+  'webdev',
+];
 
 const SkillsLayout = () => {
   return <SkillsLayoutBlock></SkillsLayoutBlock>;
 };
+
+const Loop = keyframes`
+      0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-50%);
+    }
+`;
 
 const SkillsLayoutBlock = styled.div`
   width: 100%;
--- src/pages/IndexPage/skillsSection.tsx
+++ src/pages/IndexPage/skillsSection.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Section from '../../components/common/section';
 import Title from '../../components/common/title';
+import SkillsLayout from '../../components/skills/skillsLayout';
 
 const SkillsSection = () => {
   return (
     <Section>
       <Title title={'SKILLS'} />
+      <SkillsLayout />
     </Section>
   );
 };
