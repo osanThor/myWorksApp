841b7b28ecef3a6b735d4dd2a1cadc0a8f104094
junyoung <jadw9715@gmail.com>
2023-02-06 23:47:38 +0900

    feat. side bar menu

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 import { media } from '../../../styles/theme';
 import { Logo } from '../../assets/images';
 import { useThemeContext } from '../../contexts/theme.context';
-import ToggleThemeBtn from './toggleThemeBtn';
 
 const Header = ({ children }: { children: React.ReactNode }) => {
   const theme = useThemeContext();
@@ -13,12 +13,14 @@ const Header = ({ children }: { children: React.ReactNode }) => {
     <>
       <HeaderBlock>
         <div className="logo">
-          <Image
-            src={theme === 'light' ? Logo[0] : Logo[2]}
-            alt="main logo"
-            layout="fill"
-            sizes=""
-          />
+          <Link href="/">
+            <Image
+              src={theme === 'light' ? Logo[0] : Logo[2]}
+              alt="main logo"
+              layout="fill"
+              sizes=""
+            />
+          </Link>
         </div>
         {children}
       </HeaderBlock>
--- src/containers/headerContainer.tsx
+++ src/containers/headerContainer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../components/common/header';
+import SideBar from '../components/common/sideBar';
 import ToggleThemeBtn from '../components/common/toggleThemeBtn';
 
 const HeaderContainer = ({ themeToggler }: { themeToggler: () => void }) => {
   return (
     <Header>
-      <ToggleThemeBtn themeToggler={themeToggler} />
+      <SideBar>
+        <ToggleThemeBtn themeToggler={themeToggler} />
+      </SideBar>
     </Header>
   );
 };
