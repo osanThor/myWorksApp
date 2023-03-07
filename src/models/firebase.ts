68d5590bb9dc1765638ee08732a361a84774a86d
junyoung <jadw9715@gmail.com>
2023-03-08 00:05:49 +0900

    feat: Firebase client

--- src/models/firebase.ts
+++ src/models/firebase.ts
@@ -1,4 +1,6 @@
+import { getApps, initializeApp } from 'firebase/app';
 import getConfig from 'next/config';
+import { Auth, getAuth } from 'firebase/auth';
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -7,3 +9,33 @@ const FirebaseCredential = {
   authDomain: publicRuntimeConfig.authDomain,
   projectId: publicRuntimeConfig.projectId,
 };
+
+export default class FirebaseClient {
+  private static instance: FirebaseClient;
+
+  private auth: Auth;
+
+  public constructor() {
+    const apps = getApps();
+    if (apps.length === 0) {
+      console.info('firebase client init start');
+      initializeApp(FirebaseCredential);
+    }
+    this.auth = getAuth();
+    console.info('firebase auth');
+  }
+
+  public static getInstance(): FirebaseClient {
+    if (
+      FirebaseClient.instance === undefined ||
+      FirebaseClient.instance === null
+    ) {
+      FirebaseClient.instance = new FirebaseClient();
+    }
+    return FirebaseClient.instance;
+  }
+
+  public get Auth(): Auth {
+    return this.auth;
+  }
+}
