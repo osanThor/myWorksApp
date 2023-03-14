0bbfb77225287aad818ed04bd7aa0a3894dcd88f
junyoung <jadw9715@gmail.com>
2023-03-14 23:19:16 +0900

    add: page loading component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,37 @@
 import type { AppProps } from 'next/app';
 import '../styles/fonts.css';
 import { CustomThemeProvider } from '../src/contexts/theme.context';
+import { Router } from 'next/router';
+import { useState, useEffect } from 'react';
+import PageLoading from '../src/components/common/loading/pageLoading';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [firstLoading, setfirstLoading] = useState(false);
+  const [pageLoading, setpageLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setpageLoading(true);
+    };
+    const end = () => {
+      setpageLoading(false);
+    };
+
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
   return (
     <>
       <CustomThemeProvider>
-        <Component {...pageProps} />
+        {pageLoading ? <PageLoading /> : <Component {...pageProps} />}
       </CustomThemeProvider>
     </>
   );
---
+++ src/components/common/loading/pageLoading.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const PageLoading = () => {
+  return (
+    <PageLoadingBlock>
+      <div>G</div>
+      <div>N</div>
+      <div>I</div>
+      <div>D</div>
+      <div>A</div>
+      <div>O</div>
+      <div>L</div>
+    </PageLoadingBlock>
+  );
+};
+
+const PageLoadingBlock = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export default PageLoading;
