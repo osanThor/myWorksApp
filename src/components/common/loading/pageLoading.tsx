67bae6fd11430c5357f91dc6ce85aed1ee878ebc
junyoung <jadw9715@gmail.com>
2023-03-30 00:01:20 +0900

    fix: loading component position

--- src/components/common/loading/pageLoading.tsx
+++ src/components/common/loading/pageLoading.tsx
@@ -48,16 +48,14 @@ const Move = keyframes`
 `;
 
 const PageLoadingBlock = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
   z-index: 999;
   background-color: ${({ theme }) => theme.mode.bgColor};
 
