5ea5d39f26ab81b049ee34237178ec02998db8be
junyoung <jadw9715@gmail.com>
2023-04-05 23:37:11 +0900

    feat: image loader add spinners

--- src/components/common/imageBox.tsx
+++ src/components/common/imageBox.tsx
@@ -1,5 +1,9 @@
 import Image, { StaticImageData } from 'next/image';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { ClipLoader } from 'react-spinners';
+import styled from 'styled-components';
+import colors from '../../assets/colors';
+import { useThemeContext } from '../../contexts/theme.context';
 
 interface ImageBoxProps {
   src: string | StaticImageData;
@@ -14,19 +18,36 @@ const ImageLoader = ({ src }: { src: string }) => {
 
 const ImageBox = ({ src, alt, width, height }: ImageBoxProps) => {
   const loadingRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const themeName = useThemeContext();
   return (
-    <>
+    <ImageBoxBlock>
       <Image
         loader={ImageLoader}
         src={src ? src : ''}
         alt={alt}
         width={width}
         height={height}
-        onLoadingComplete={() => loadingRef.current?.remove()}
+        onLoadingComplete={() => setLoading(false)}
       />
-      <div className="loading" ref={loadingRef} />
-    </>
+      <ClipLoader
+        color={themeName === 'dark' ? colors.red[1] : colors.blue[1]}
+        loading={loading}
+        size={50}
+        ref={loadingRef}
+      />
+    </ImageBoxBlock>
   );
 };
+
+const ImageBoxBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  & > span {
+    position: absolute;
+    border-color: ${({ theme }) => theme.mode.mainColor};
+  }
+`;
 
 export default ImageBox;
