6cad34ce34fa75af1a4cef52ecadd3852aca96a1
junyoung <jadw9715@gmail.com>
2023-04-12 23:16:45 +0900

    refactor. app 정리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/fonts.css';
 import { CustomThemeProvider } from '../src/contexts/theme.context';
-import { Router } from 'next/router';
-import { useState, useEffect } from 'react';
-import PageLoading from '../src/components/common/loading/pageLoading';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
