ac6d689516c25975c5221ab6a373b95b8b9a7c77
junyoung <jadw9715@gmail.com>
2023-06-06 22:45:32 +0900

    feat. sidebare menu home btn

--- src/components/common/sideBar.tsx
+++ src/components/common/sideBar.tsx
@@ -25,6 +25,11 @@ const SideBar = ({ children }: { children: React.ReactNode }) => {
         <span className="spacer" />
         <ul className="gnb">
           <li>
+            <Link href="/">
+              <span className="txt">HOME</span>
+            </Link>
+          </li>
+          <li>
             <Link href="/works">
               <span className="txt">WORKS</span>
             </Link>
